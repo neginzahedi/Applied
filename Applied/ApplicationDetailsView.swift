5c5b1fd620584cce1bf6a5bf0fecc95e241519c4
Negin Zahedi <zahedi.negin@gmail.com>
2024-03-28 13:44:25 -0400

    Implement a view to display application details and update the status

--- Applied/ApplicationDetailsView.swift
+++ Applied/ApplicationDetailsView.swift
@@ -8,34 +8,90 @@ import CoreData
 
 struct ApplicationDetailsView: View {
     
+    // MARK: - Properties
+    
+    @Environment(\.managedObjectContext) var managedObjectContext
+    @Environment(\.dismiss) var dismiss
+    
     var application: Application
+    @State private var applicationStatus: String
+    private var applicationStatuses = ["Received", "Under Review", "Interview Scheduled", "Interviewed", "Pending Decision", "Offer Extended", "Offer Accepted", "Offer Declined", "Not Selected", "Withdrawn"]
+    
+    // MARK: - init
+    init(application: Application){
+        self.application = application
+        self.applicationStatus = application.applicationStatus!
+    }
+    
+    // MARK: - Body
     
     var body: some View {
-        VStack(alignment: .leading){
-            Text(application.jobTitle ?? "job title")
-                .font(.title)
-                .bold()
-            Label(application.companyName ?? "Company's Name", systemImage: "building.2.fill")
-            Label(application.city ?? "Location", systemImage: "mappin")
-            Label((application.employmentType ?? "employment type") + " . " + (application.workMode ?? "workmode") , systemImage: "briefcase.fill")
-            Label(Utils.formatDateToMonthDayYear(application.dateApplied!), systemImage: "calendar")
-            Label(application.applicationStatus ?? "Status", systemImage: "clock.arrow.2.circlepath")
-            Divider()
-            
-            VStack{
-                Text("Note:")
+        List{
+            Section{
+                Label(application.companyName ?? "Company's Name", systemImage: "building.2.fill")
+                Label(application.city ?? "Location", systemImage: "mappin")
+                Label((application.employmentType ?? "employment type") + " . " + (application.workMode ?? "workmode") , systemImage: "briefcase.fill")
+                Label(Utils.formatDateToMonthDayYear(application.dateApplied ?? Date()), systemImage: "calendar")
+            } header: {
+                Text("Job information")
             }
             
-            Button("Edit"){
-                //
+            Section{
+                Text("vfjhvbsljxdn;lwnmx;lnwklndxklwbxdjkwbxjkw kewnboibew ceiwnixoew cwenhpiocwhe cpekwopckwe lckwbnc ")
+                
+            } header: {
+                Text("Note")
             }
-            Button("Delete"){
-                //
+            
+            Section{
+                Picker("Current Status", selection: $applicationStatus) {
+                    ForEach(applicationStatuses, id: \.self) { status in
+                        Text(status)
+                            .tag(status)
+                    }
+                }
+            } header: {
+                Text("Application Status")
             }
-            Spacer()
+            
+            Section{
+                Button("Delete"){
+                    // TODO: fix warning
+                    deleteApplication(application: application)
+                }
+                .tint(.red)
+            }
         }
-        .padding()
+        .listStyle(.insetGrouped)
         
-        .navigationTitle(application.jobTitle!)
+        .onDisappear(){
+            // TODO: fix warning
+            saveChanges()
+        }
+        .navigationTitle(application.jobTitle ?? "Job title")
+    }
+    
+    // MARK: - Methods
+    
+    private func saveChanges() {
+        application.applicationStatus = applicationStatus
+        do {
+            try managedObjectContext.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+    }
+    
+    private func deleteApplication(application: Application) {
+        managedObjectContext.delete(application)
+        do {
+            try managedObjectContext.save()
+            print("Deleted application")
+            dismiss()
+        } catch {
+            // Handle the Core Data error
+            print("Failed to delete application: \(error.localizedDescription)")
+        }
     }
 }
