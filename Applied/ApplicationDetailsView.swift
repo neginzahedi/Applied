856ace31a1c6b5bbc761edcaf808e01e1969f891
Negin Zahedi <zahedi.negin@gmail.com>
2024-04-14 14:52:13 -0400

    Refactor ApplicationDetailsView

--- Applied/ApplicationDetailsView.swift
+++ Applied/ApplicationDetailsView.swift
@@ -13,52 +13,25 @@ struct ApplicationDetailsView: View {
     @Environment(\.managedObjectContext) var managedObjectContext
     @Environment(\.dismiss) var dismiss
     
-    var application: Application
+    @ObservedObject var application: Application
     @State var applicationStatus: String
     @State private var showConfirmation: Bool = false
     
     // MARK: - Body
     
     var body: some View {
-        List{
-            Section {
-                Label(application.company ?? "Company", systemImage: "building.2.fill")
-                Label(application.location ?? "Location" , systemImage: "mappin")
-                Label((application.employmentType ?? "Employment Type") + " . " + (application.workMode ?? "Work Mode") , systemImage: "briefcase.fill")
-                Label(Utils.formatDateToMonthDayYear(application.dateApplied ?? Date()), systemImage: "calendar")
-            } header: {
-                Text("Job information")
+        VStack(spacing: 15){
+            jobTitleView()
+            applicationInfoView()
+            if let note = application.note, !note.isEmpty {
+                noteView(note: note)
             }
-            
-            Section {
-                Text(application.note ?? "Note")
-                
-            } header: {
-                Text("Note")
-            }
-            
-            Section {
-                Picker("Current Status", selection: $applicationStatus) {
-                    ForEach(Constants.applicationStatuses, id: \.self) { status in
-                        Text(status)
-                            .tag(status)
-                    }
-                }
-                .onChange(of: applicationStatus) {
-                    saveChanges()
-                }
-            } header: {
-                Text("Application Status")
-            }
-            
-            Section {
-                Button("Delete"){
-                    showConfirmation.toggle()
-                }
-                .tint(.red)
-            }
+            statusPicker()
+            Spacer()
+            actionButtons()
         }
-        .listStyle(.insetGrouped)
+        .padding()
+        
         
         .confirmationDialog("Delete Application",
                             isPresented: $showConfirmation,
@@ -72,30 +45,158 @@ struct ApplicationDetailsView: View {
             Text("Are you sure you want to delete \(application.jobTitle ?? "this") application?")
         }
         
-        .navigationTitle(application.jobTitle ?? "Job title")
+        .navigationTitle("Application Details")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+    
+    // MARK: - Views
+    
+    private func jobTitleView() -> some View {
+        HStack{
+            Spacer()
+            Text(application.jobTitle ?? "Job Title")
+                .font(.headline)
+                .bold()
+            Spacer()
+        }
+        .modifier(RoundedRectangleModifier())
+        .background(.customYellow, in: RoundedRectangle(cornerRadius: 10))
+    }
+    
+    private func applicationInfoView() -> some View {
+        VStack(alignment: .center, spacing: 10){
+            VStack(spacing: 25){
+                infoView(title: "Company", text: application.company ?? "company")
+                infoView(title: "Location", text: application.location ?? "Location")
+                infoView(title: "Employment Type", text: application.employmentType ?? "Employment Type")
+                infoView(title: "Work Mode", text: application.workMode ?? "Work Mode")
+                infoView(title: "Date Applied", text: Utils.formatDateToMonthDayYear(application.dateApplied ?? Date()))
+            }
+        }
+        .modifier(RoundedRectangleModifier())
+        .background(.white, in: RoundedRectangle(cornerRadius: 10))
+        
+    }
+    
+    private func infoView(title: String, text: String) -> some View {
+        HStack{
+            Text(title)
+                .foregroundStyle(.secondary)
+            Spacer()
+            Text(text)
+        }
+    }
+    
+    private func noteView(note: String) -> some View {
+        VStack(alignment: .leading){
+            HStack{
+                Text("Note:")
+                Spacer()
+            }
+            Text(note)
+        }
+        .modifier(RoundedRectangleModifier())
+        .background(.white, in: RoundedRectangle(cornerRadius: 10))
+    }
+    
+    private func statusPicker() -> some View{
+        HStack{
+            Text("Application Status")
+                .foregroundStyle(.secondary)
+            Spacer()
+            Picker("Status", selection: $applicationStatus) {
+                ForEach(Constants.applicationStatuses, id: \.self) { status in
+                    Text(status)
+                        .tag(status)
+                }
+            }
+            .tint(.primary)
+            .onChange(of: applicationStatus) {
+                saveChanges()
+            }
+        }
+        .modifier(RoundedRectangleModifier())
+        .background(.white, in: RoundedRectangle(cornerRadius: 10))
+    }
+    
+    private func actionButtons() -> some View{
+        HStack(spacing: 15){
+            Button(action: {
+                // TODO: Edit applications, navigate to edit view
+                print("edited")
+            }, label: {
+                HStack{
+                    Spacer()
+                    Text("Edit")
+                    Spacer()
+                }
+                .modifier(RoundedRectangleModifier())
+                .background(.customBlue, in: RoundedRectangle(cornerRadius: 10))
+                .foregroundColor(.black)
+                .font(.headline)
+                .fontDesign(.rounded)
+            })
+            
+            Button(action: {
+                showConfirmation.toggle()
+            }, label: {
+                HStack{
+                    Spacer()
+                    Text("Delete")
+                    Spacer()
+                }
+                .modifier(RoundedRectangleModifier())
+                .background(.customPink, in: RoundedRectangle(cornerRadius: 10))
+                .foregroundColor(.black)
+                .font(.headline)
+                .fontDesign(.rounded)
+            })
+        }
+        .padding(.bottom, 10)
     }
     
     // MARK: - Methods
     
     private func saveChanges() {
-        application.applicationStatus = applicationStatus
-        do {
-            try self.managedObjectContext.save()
-            print("Application status updated.")
-        } catch {
-            fatalError("Error updating status: \(error.localizedDescription)")
+        guard let context = application.managedObjectContext else {
+            fatalError("Application's managed object context is nil.")
+        }
+        
+        context.perform {
+            do {
+                application.applicationStatus = applicationStatus
+                try context.save()
+                print("Application status updated.")
+            } catch {
+                print("Error updating application status: \(error.localizedDescription)")
+            }
         }
     }
     
     private func deleteApplication(application: Application) {
-        managedObjectContext.delete(application)
-        do {
-            try managedObjectContext.save()
-            print("Application deleted.")
-            dismiss()
-        } catch {
-            // Handle the Core Data error
-            print("Failed to delete application: \(error.localizedDescription)")
+        guard let context = application.managedObjectContext else {
+            fatalError("Application's managed object context is nil.")
+        }
+        
+        context.perform {
+            context.delete(application)
+            
+            do {
+                try context.save()
+                print("Application deleted.")
+                
+                DispatchQueue.main.async {
+                    dismiss()
+                }
+            } catch {
+                print("Failed to delete application: \(error.localizedDescription)")
+            }
         }
     }
 }
+
+
+
+//#Preview {
+//    ApplicationDetailsView()
+//}
